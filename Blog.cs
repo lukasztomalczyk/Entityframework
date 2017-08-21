99f958ba5b3352b63b1e3cc22558f1843a435d83
Łukasz Tomalczyk <lukasz.tomalczyk@onet.pl>
2017-08-21 21:11:07 +0200

    pierwszy commit

---
+++ Blog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnityFramework
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+
+        public virtual List<Post> Posts { get; set; }
+    }
+}
---
+++ BloggingContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Entity;
+
+namespace EnityFramework
+{
+    class BloggingContext : DbContext
+    {
+        public BloggingContext()
+        {
+            
+        }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+    }
+}
